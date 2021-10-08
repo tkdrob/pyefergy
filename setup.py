c9c7d30c1dfd2ca9be9aa2c279139a9baf313da7
Robert Hillis <tkdrob4390@yahoo.com>
2021-10-08 09:02:07 -0400

    bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pyefergy',
-    version='0.1.1',
+    version='0.1.2',
     author='Robert Hillis',
     author_email='tkdrob4390@yahoo.com',
     description='An API library for Efergy energy meters.',
